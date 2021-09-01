f0d60a9e54f2c2835219dfa744f2e47c256fbc9c
3minhaz <3minhaz@gmail.com>
2021-09-02 00:56:33 +0600

    title,cover and author name added

--- main.js
+++ main.js
@@ -8,11 +8,29 @@ searchButton.addEventListener('click', function () {
         .then(data => displayBooks(data.docs));
 })
 const displayBooks = (books) => {
+    console.log(books);
+    // console.log(book.author_name[0]);
     books.forEach(book => {
-        // console.log(book.author_name);
+        console.log(book.cover_i);
+        // console.log(book.title);
+        // console.log(book.author_name[0]);
+        // console.log(book.first_publish_year);
         const bookName = book.author_name;
         // bookName.forEach(bookName => {
         //     console.log(bookName)
         // })
+        const booksDiv = document.getElementById('books-div');
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                <img width='100px' height='300px' src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">title: ${book.title}</h5>
+                    <p class="card-text">Author: ${book.author_name}</p>
+                </div>
+            </div>
+        `
+        booksDiv.appendChild(div);
     })
 }
